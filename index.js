65bdb4dd33f9e5fb98bc332243f045c7185cd20d
VirtualHoriz0n <83425025+VirtualHoriz0n@users.noreply.github.com>
2021-06-10 20:04:48 +0100

    Fixed formatting in index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-//Requires the discord.js module
+//  Requires the discord.js module
 const Discord = require('discord.js');
-//Creates a new Discord client
+//  Creates a new Discord client
 const client = new Discord.Client();
 
-//When the client is ready, run this code
-//This code will only trigger once, during the login.
+//  When the client is ready, run this code
+//  This code will only trigger once, during the login.
 client.once('ready', () => {
-    console.log('Ready!')
+	console.log('Ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
-        //sends back "Pong." to channel the message was sent in
-        message.channel.send('Pong.');
-    }
+	if (message.content === '!ping') {
+		//  sends back "Pong." to channel the message was sent in
+		message.channel.send('Pong.');
+	}
 });
 
 
-
-
-
-//Log in using the token
+//  Log in using the token
 client.login('ODUyNjAxOTQ2OTc5MTcyMzgz.YMJNcA.PU9fpKqOeH4a8Pl_luShGgGQmIc');
