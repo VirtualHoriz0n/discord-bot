d8811663994eeb74d4ba875d9fef2e45837999a5
VirtualHoriz0n <83425025+VirtualHoriz0n@users.noreply.github.com>
2021-06-10 19:51:43 +0100

    Initial Upload of discord-bot files

---
+++ index.js
@@ -0,0 +1,24 @@
+//Requires the discord.js module
+const Discord = require('discord.js');
+//Creates a new Discord client
+const client = new Discord.Client();
+
+//When the client is ready, run this code
+//This code will only trigger once, during the login.
+client.once('ready', () => {
+    console.log('Ready!')
+});
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+        //sends back "Pong." to channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
+
+
+
+
+//Log in using the token
+client.login('ODUyNjAxOTQ2OTc5MTcyMzgz.YMJNcA.PU9fpKqOeH4a8Pl_luShGgGQmIc');
